ab7beb655da8604fef4a6599214170dec95393aa
Arian Derida <arianderida@gmail.com>
2022-03-09 07:02:25 +0700

    feat: setup public and private route :lock:

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,23 @@
 const auth = async ({ app, route, redirect, store }) => {
-  // const publish =
-  //   (route?.name || '').includes('auth') || (route?.name || '') === 'index'
-  // const auth = (route?.name || '').includes('home')
-  // if (publish) {
-  //   // const token = app.$cookiz.get('x-csrf-token')
-  // const user = await store.dispatch('session/user')
-  //   console.log(user)
-  //   return true
-  // }
-  // return true
+  const publish =
+    (route?.name || '').includes('auth') || (route?.name || '') === 'index'
+  const auth = (route?.name || '').includes('home')
+  const token = app.$cookiz.get('x-csrf-token')
+
+  if (auth) {
+    if (!token) {
+      return redirect('/')
+    }
+  }
+
+  if (publish) {
+    if (token) {
+      const user = await store.dispatch('session/user')
+
+      if (user) {
+        return redirect('/home')
+      }
+    }
+  }
 }
 export default auth
